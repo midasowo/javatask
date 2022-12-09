418b18b2524fd25ab679fa4ffbb9bd3e86ab8e13
midasowo <kmiduch@gmail.com>
2022-12-09 17:50:05 +0100

    Added some fields to class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
+    static String room1 = "Pokój 1";
+    static String room2 = "Pokój 2";
+    static String room3 = "Pokój 3";
+
     public static void main(String[] args) {
+        String text = "Jestem tekstem";
+        int number = 12;
+        double number2 = 2.2;
+
+        System.out.println("Zmienna z treścią: " + text);
+        System.out.println("Liczba całkowita: " + number);
+        System.out.println("Liczba zmiennoprzecinkowa: " + number2);
 
 
     }
